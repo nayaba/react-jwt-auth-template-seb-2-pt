12f448ac0aaa33fc6f8792139926c82b4c2ad58d
Nabila Ayaba <nabilaayaba@gmail.com>
2025-02-10 19:02:14 +0300

    Add Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router'
 import NavBar from './components/NavBar/NavBar'
 import SignUpForm from './components/SignUpForm/SignUpForm'
+import Landing from './components/Landing/Landing'
 
 const App = () => {
   return (
     <>
       <NavBar />
       <Routes>
+        <Route path='/' element={<Landing />} />
         <Route path='/sign-up' element={<SignUpForm />} />
       </Routes>
     </>
---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,9 @@
+const Landing = () => {
+    return (
+        <main>
+            <h1>Landing Page</h1>
+        </main>
+    )
+}
+
+export default Landing
