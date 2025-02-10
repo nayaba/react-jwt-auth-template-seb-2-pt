3ce01da729437ec5c7778b8f755aa51350f039e4
Nabila Ayaba <nabilaayaba@gmail.com>
2025-02-10 18:57:51 +0300

    Create sign up form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from 'react-router'
 import NavBar from './components/NavBar/NavBar'
+import SignUpForm from './components/SignUpForm/SignUpForm'
 
 const App = () => {
   return (
     <>
       <NavBar />
+      <Routes>
+        <Route path='/sign-up' element={<SignUpForm />} />
+      </Routes>
     </>
   )
 }
